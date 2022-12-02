7720e4a793a3c37df32bcdc17465c3d984e93ef6
Isabel Palacios <issabelpalacios@gmail.com>
2022-12-02 17:06:32 -0300

    ADD: listado de episodios

--- src/pages/episodios/Episodios.jsx
+++ src/pages/episodios/Episodios.jsx
@@ -1,5 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import getEpisodes from "../../api/getEpisodes";
 
 export default function Episodios() {
-  return <div>Episodios</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function showData() {
+      const resp = await getEpisodes();
+      setData(resp);
+      console.log(resp);
+      return resp;
+    }
+    showData();
+  }, []);
+  return (
+    <div className="layoutpage custom">
+      <div className="episopdes">
+        {data.map((episopde) => (
+          <div key={episopde.episode_id}>
+            <p>ID: {episopde.episode_id}</p>
+            <p>Title: {episopde.title}</p>
+            <p>Season: {episopde.season}</p>
+            <p>Air date: {episopde.air_date}</p>
+            <p>Characters; {episopde.characters.toString()}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
