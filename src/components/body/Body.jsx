bf125dcd4f44942e41b1a3bf3ca14499c326c01c
Miguel Jara <miguel.jara.rodriguez@gmail.com>
2022-12-03 20:10:42 -0300

    Se agrega background
    - Se agrega background al main del portal

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import background from "../../assets/img/bg.jpg";
+
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
 
@@ -15,14 +17,16 @@ export default function Body() {
   return (
     <BrowserRouter>
         <Header />
-        <Routes>
-            <Route path='/personajes' element={<Characters />} />
-            <Route path='/episodios' element={<Episodes />} />
-            <Route path='/citas' element={<Quotes />} />
-            <Route path='/muertes' element={<Deaths />} />
-            <Route path='/' element={<Home />} />
-            <Route path='*' element={<NotFound />} />
-        </Routes>
+        <main style={{ backgroundImage: `url(${background})`, backgroundRepeat: `no-repeat`, backgroundAttachment: `fixed`, backgroundSize: `cover` }}>
+          <Routes>
+              <Route path='/personajes' element={<Characters />} />
+              <Route path='/episodios' element={<Episodes />} />
+              <Route path='/citas' element={<Quotes />} />
+              <Route path='/muertes' element={<Deaths />} />
+              <Route path='/' element={<Home />} />
+              <Route path='*' element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
     </BrowserRouter>
   )
