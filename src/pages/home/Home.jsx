0f6a84655529f9b79322ea00e3a2a825a2f72c5c
Isabel Palacios <issabelpalacios@gmail.com>
2022-12-04 19:54:41 -0300

    home en español

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,11 @@ import bbimg from '../../assets/img/bbimg.jpg'
 export default function Home() {
   return <div className="p-5">Home 
  <div className="pb-5">
-    <h1 className="fs-1 text-light text-center">The real fandom site of Breaking Bad is here...!!!</h1>
+    <h1 className="fs-1 text-light text-center">El mejor site para el fandom de Breaking Bad esta aquí...!!!</h1>
     </div>
     <div className="px-1">
-      <p className="text-light text-center fs-3">Visit us to learn about episodes, funny phrases, wise phrases, 
-      the most painful deaths and the best characters from your favorite series, all on our site </p>
+      <p className="text-light text-center fs-3">Visitanos y conoce todo sobre la serie de televisión estadounidense 
+      de drama criminal creada y producida por Vince Gilligan, personajes, citas, y las mas dolorosas muertes podrás revivirlas aqui!</p>
   <div className="px-5">
   <div className="px-5">
     <img src={bbimg} alt="Breaking Bad" className="img-fluid" />
